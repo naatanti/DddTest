7ba98fd1985192b945562f3733749e4d2583360b
NATAN SOUZA <NATAN.SOUZA@ATELIETCERO.LOCAL>
2018-05-17 13:41:24 -0400

    Add project files.

---
+++ ValidadorPessoal.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ValidadorPessoal.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ValidadorPessoal.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c92cc0a9-2390-499a-8357-e6941ce8bf22")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Web_App/Controllers/EnviarArquivoXMLController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace Web_App.Controllers
+{
+    public class EnviarArquivoXMLController : Controller
+    {
+        // GET: EnviarArquivoXML
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Enviar(HttpPostedFileBase arquivo)
+        {
+            XmlDocument xml = new XmlDocument();
+
+            return View();
+        }
+    }
+}
